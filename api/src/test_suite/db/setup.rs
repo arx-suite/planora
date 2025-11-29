fa5849fd793a678c85799eec1a2a7dcfb46dc6b9
sajeevan <sajeevanjspy@gmail.com>
2025-11-29 12:47:00 +0530

    test: add role management setup with initial SQL script - introduces new database roles - added initial SQL bootstrap script to create roles, grant privileges, and manage schema ownership

--- api/src/test_suite/db/setup.rs
+++ api/src/test_suite/db/setup.rs
@@ -11,18 +11,41 @@ const TESTCONTAINER_CONTAINER_NAME: &'static str = "arx-testhouse";
 const PG_MAX_CONNECTIONS: u32 = 3;
 const DB_HOST: &'static str = "localhost";
 const DB_PORT: u16 = 5432;
-const DB_USER_ADMIN: &'static str = "pg-test-admin";
-const DB_PASS_ADMIN: &'static str = "pg-test-admin-pass";
-const DB_USER_USER1: &'static str = "pg-test-user1";
-const DB_PASS_USER1: &'static str = "pg-test-user1-pass";
+const DB_USER_ADMIN: &'static str = "admin";
+const DB_PASS_ADMIN: &'static str = "admin-se3ret";
+const DB_USER_RW: &'static str = "rw";
+const DB_PASS_USER_RW: &'static str = "rw-se3ret";
+const DB_USER_MIGRATOR: &'static str = "migrator";
+const DB_PASS_USER_MIGRATOR: &'static str = "migrator-se3ret";
 const DB_NAME: &'static str = "pg-test-db";
+
+const INIT_SCRIPT: &'static str = r#"
+-- roles
+CREATE ROLE schema_owner NOLOGIN;
+CREATE ROLE migrator LOGIN PASSWORD 'migrator-se3ret';
+CREATE ROLE rw LOGIN PASSWORD 'rw-se3ret';
+
+GRANT schema_owner TO migrator;
+GRANT schema_owner TO rw;
+
+ALTER ROLE migrator SET ROLE = schema_owner;
+
+-- schema ownership
+ALTER SCHEMA public OWNER TO schema_owner;
+
+-- default grants
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO rw;
+"#;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub struct TestDb {
-    pg_container: ContainerAsync<PostgresContainer>,
-    admin_pool: PgPool,
-    // user1_pool: PgPool,
+    pub pg_container: ContainerAsync<PostgresContainer>,
+    pub admin_pool: PgPool,
+    pub rw_pool: PgPool,
 }
 
 impl TestDb {
@@ -30,17 +53,19 @@ impl TestDb {
         let pg_container = Self::container_setup().await?;
         let port = pg_container.get_host_port_ipv4(DB_PORT).await?;
 
-        let admin_pool = Self::get_admin_pool(port).await?;
+        let migrator_pool = Self::get_migrator_pool(port).await?;
         sqlx::migrate!("./migrations")
-            .run(&admin_pool)
+            .run(&migrator_pool)
             .await
             .expect("migrations failed");
 
-        // let user1_pool = Self::get_user1_pool(port).await?;
+        let admin_pool = Self::get_admin_pool(port).await?;
+        let rw_pool = Self::get_rw_pool(port).await?;
 
         Ok(Self {
             pg_container,
             admin_pool,
+            rw_pool,
         })
     }
 
@@ -56,6 +81,7 @@ impl TestDb {
 
     async fn container_setup() -> Result<ContainerAsync<PostgresContainer>> {
         let container = PostgresContainer::default()
+            .with_init_sql(INIT_SCRIPT.to_string().into_bytes())
             .with_user(DB_USER_ADMIN)
             .with_password(DB_PASS_ADMIN)
             .with_db_name(DB_NAME)
@@ -70,24 +96,28 @@ impl TestDb {
     pub async fn get_admin_pool(port: u16) -> Result<PgPool> {
         let admin_database_url =
             format!("postgres://{DB_USER_ADMIN}:{DB_PASS_ADMIN}@{DB_HOST}:{port}/{DB_NAME}");
-
-        let pool = PgPoolOptions::new()
-            .max_connections(PG_MAX_CONNECTIONS)
-            .connect(&admin_database_url)
-            .await?;
-
-        Ok(pool)
+        Self::pool(&admin_database_url).await
     }
 
-    pub async fn get_user1_pool(port: u16) -> Result<PgPool> {
+    pub async fn get_rw_pool(port: u16) -> Result<PgPool> {
         let admin_database_url =
-            format!("postgres://{DB_USER_USER1}:{DB_PASS_USER1}@{DB_HOST}:{port}/{DB_NAME}");
+            format!("postgres://{DB_USER_RW}:{DB_PASS_USER_RW}@{DB_HOST}:{port}/{DB_NAME}");
+        Self::pool(&admin_database_url).await
+    }
 
+    pub async fn get_migrator_pool(port: u16) -> Result<PgPool> {
+        let admin_database_url = format!(
+            "postgres://{DB_USER_MIGRATOR}:{DB_PASS_USER_MIGRATOR}@{DB_HOST}:{port}/{DB_NAME}"
+        );
+        Self::pool(&admin_database_url).await
+    }
+
+    #[inline]
+    async fn pool(url: &str) -> Result<PgPool> {
         let pool = PgPoolOptions::new()
             .max_connections(PG_MAX_CONNECTIONS)
-            .connect(&admin_database_url)
+            .connect(&url)
             .await?;
-
         Ok(pool)
     }
 }
