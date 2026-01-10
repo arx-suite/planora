f69a96e2aee78bda5bc6048fda9157f0497abd3c
sajeevan <sajeevanjspy@gmail.com>
2026-01-10 17:56:39 +0530

    fix: aligned config variables with the env variables - updated the dependent code

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    api: process.env.NEXT_PUBLIC_API!,
-    nextjs: process.env.NEXT_PUBLIC_BASE_URL!,
-    internal_api: process.env.ARX_PLANORA_API!,
+    api: process.env.NEXT_PUBLIC_API || "http://localhost:8080",
+    nextjs: process.env.NEXT_PUBLIC_BASE_URL || "http://planora.sbs:3000",
+    domain: process.env.NEXT_DOMAIN || "planora.sbs",
 };
