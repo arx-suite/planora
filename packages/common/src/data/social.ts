73e30d23a57a18bb28ecf795700de4744b56c046
sajeevan <sajeevanjspy@gmail.com>
2026-01-21 20:11:30 +0530

    feat: add social links

---
+++ packages/common/src/data/social.ts
@@ -0,0 +1,13 @@
+const GITHUB = "https://github.com/arx-suite/planora";
+
+export const social = {
+    github: {
+        org: GITHUB,
+        issues: `${GITHUB}/issues`,
+    },
+};
+
+export const maintainer = {
+    name: "sajeevan",
+    github: "https://github.com/sajeevanjspy",
+};
