d4bb56a14216d872a1375d409b73653fd265d912
sajeevan <sajeevanjspy@gmail.com>
2026-01-30 14:26:41 +0530

    chore: export lucide react icons from the ui package

--- packages/ui/src/icons/index.ts
+++ packages/ui/src/icons/index.ts
@@ -1,3 +1,4 @@
-export * from "./activity-icon";
-export * from "./layout-grid-icon";
-export * from "./settings-icon";
+export * from "lucide-react";
+export { ActivityIcon } from "./activity-icon";
+export { LayoutGridIcon } from "./layout-grid-icon";
+export { SettingsIcon } from "./settings-icon";
