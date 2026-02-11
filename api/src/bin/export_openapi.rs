93fb0fcd90c68b1d20160b0a5ac959d1e04f577b
sajeevan <sajeevanjspy@gmail.com>
2026-02-11 11:08:15 +0530

    feat: updated export_openapi binary - changed the generated openapi file format from json to yaml

--- api/src/bin/export_openapi.rs
+++ api/src/bin/export_openapi.rs
@@ -2,13 +2,19 @@ use arx_gatehouse::ApiDoc;
 use std::fs;
 use utoipa::OpenApi;
 
+const OPENAPI_FOLDER: &str = "openapi";
+const OPENAPI_FILE: &str = "openapi.yaml";
+
 fn main() {
     let spec = ApiDoc::openapi()
-        .to_pretty_json()
+        .to_yaml()
         .expect("Failed to serialize OpenAPI");
 
-    fs::create_dir_all("openapi").unwrap();
-    fs::write("openapi/openapi.json", spec).unwrap();
+    let api_file = format!("{OPENAPI_FOLDER}/{OPENAPI_FILE}");
 
-    println!("OpenAPI spec written to openapi/openapi.json");
+    fs::create_dir_all(OPENAPI_FOLDER).expect("Failed to create spec folder");
+    fs::write(format!("{OPENAPI_FOLDER}/{OPENAPI_FILE}"), spec)
+        .expect("Failed to create openapi file");
+
+    println!("OpenAPI spec written to {api_file}");
 }
