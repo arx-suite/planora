053d3d4df21c095314ad3187d29bef0492269218
sajeevan <sajeevanjspy@gmail.com>
2026-02-19 14:11:19 +0530

    chore: changed API url in config

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    api: process.env.NEXT_PUBLIC_API || "http://localhost:8080",
+    api: process.env.NEXT_PUBLIC_API || "http://api.planora.sbs:8080",
     nextjs: process.env.NEXT_PUBLIC_BASE_URL || "http://planora.sbs:3000",
     domain: process.env.NEXT_DOMAIN || "planora.sbs",
 };
